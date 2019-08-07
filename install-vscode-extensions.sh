b5884743a998e9a1435ab4eeed0fa02ff1187bc1
mrdefenestrator <mike-g@comcast.net>
2019-08-07 15:38:42 -0600

    Add script for setting up vscode

---
+++ install-vscode-extensions.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# Installs my extensions for VSCode
+set -eu
+
+# code --list-extensions
+
+code --install-extension Arjun.swagger-viewer
+code --install-extension austin.code-gnu-global
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension dawhite.mustache
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension donjayamanne.githistory
+code --install-extension eamodio.gitlens
+code --install-extension ed-elliott.azure-arm-template-helper
+code --install-extension eg2.tslint
+code --install-extension eg2.vscode-npm-script
+code --install-extension esbenp.prettier-vscode
+code --install-extension glen-84.sass-lint
+code --install-extension humao.rest-client
+code --install-extension JuanBlanco.solidity
+code --install-extension kisstkondoros.vscode-codemetrics
+code --install-extension mauve.terraform
+code --install-extension mikestead.dotenv
+code --install-extension ms-azure-devops.azure-pipelines
+code --install-extension ms-azuretools.vscode-azureappservice
+code --install-extension ms-azuretools.vscode-azurefunctions
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+code --install-extension ms-mssql.mssql
+code --install-extension ms-python.python
+code --install-extension ms-vscode.azure-account
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.csharp
+code --install-extension ms-vscode.Go
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension msazurermtools.azurerm-vscode-tools
+code --install-extension natewallace.angular2-inline
+code --install-extension numaru.vscode-tap
+code --install-extension oderwat.indent-rainbow
+code --install-extension quantum.quantum-devkit-vscode
+code --install-extension redhat.vscode-yaml
+code --install-extension sourcegraph.javascript-typescript
+code --install-extension timonwong.shellcheck
+code --install-extension vncz.vscode-apielements
+code --install-extension yzhang.markdown-all-in-one
